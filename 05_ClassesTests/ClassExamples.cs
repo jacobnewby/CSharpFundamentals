f6077b8ae217cb24ce5dcf07e2ca7dc17b68ba33
jacobnewby <jacobnewby01@gmail.com>
2019-09-18 15:48:14 -0400

    class update

---
+++ 05_ClassesTests/ClassExamples.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using _05_Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _05_ClassesTests
+{
+    [TestClass]
+    public class ClassExamples
+    {
+        [TestMethod]
+        public void CookieClassInstantiation()
+        {
+            Cookie cookie = new Cookie();
+
+            cookie.Name = "SnickerDoodle";
+            cookie.Ingredient = "sugar";
+            cookie.BatchSize = 122;
+            cookie.Direction = "Good Luck omegaLul";
+            cookie.Shape = "Diamond";
+            cookie.Size = "Small";
+
+            Cookie cookie2 = new Cookie();
+
+            cookie2.Name = "Triple Chocolate Fudge";
+            cookie2.Ingredient = "Chocolate, dumby";
+            cookie2.BatchSize = 1;
+            cookie2.Direction = "It's a secret. I will never tell you.";
+            cookie2.Shape = "Moby Dick";
+            cookie2.Size = "ninety feet";
+
+            Cookie cookieThree = new Cookie("Peanut Butter", "Don't be stupid.", "Too big for you.", "Do it yourself.", "Peanut", 128);
+        }
+        [TestMethod]
+        public void Veh()
+        {
+            Vehicle vehicle = new Vehicle();
+
+            vehicle.Color = "Tan";
+            vehicle.CoolnessFactor = 6;
+            vehicle.IsRunning = true;
+            vehicle.Make = "Buick";
+            vehicle.Mileage = 200000d;
+            vehicle.Model = "Park Avenue";
+            vehicle.VIN = "idk";
+            vehicle.Year = 2000;
+
+            Vehicle vehicle2 = new Vehicle("Buick", "Park Avenue", 200000d, "Tan", "idk", 2000, 6, true);
+        }
+    }
+}
---
+++ 05_ClassesTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("05_ClassesTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("05_ClassesTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0728dadc-09d4-43bf-bf03-244ef171c585")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
